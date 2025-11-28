b0a02cab60912f26747ad5f86f7ed4bb1a570e78
Andros178 <serdaniel01@gmail.com>
2025-11-28 12:27:22 -0500

    Fix: Imagenes nombres normalizados

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
-import iconNavbar from "../assets/huilapp_imagotipo.png";
+import iconNavbar from "../assets/huilappImagotipo.png";
 import { FiHome, FiMapPin, FiMap, FiMessageSquare, FiUser, FiPlus, FiLogOut } from "react-icons/fi";
 
 const parseJwt = (token) => {
