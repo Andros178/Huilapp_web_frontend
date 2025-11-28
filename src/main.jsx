a2d64dd9f1b764ddbead09e39c10ef384a1bbb5f
Andres <119151057+Andros178@users.noreply.github.com>
2025-11-28 13:12:46 -0500

    Cambio de mani

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>VERSION DESPLIEGUE PRUEBA</h1>
     <App />
   </React.StrictMode>
 );
