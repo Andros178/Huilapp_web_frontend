9faff259789385315d926474d993d7ec05f68b94
Andros178 <serdaniel01@gmail.com>
2025-11-28 00:09:33 -0500

    Fix: Errores de mapa y diseño, Add: Nueva fuente para proyecto (Poppins)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { AuthProvider } from "./context/AuthContext";
 import AppRouter from "./routes/AppRouter";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root { height: 100%; }
+  body {
+    margin: 0;
+    font-family: 'Poppins', system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: #f7f7f7;
+    color: #222;
+  }
+  * { box-sizing: border-box; }
+`;
 
 function App() {
   return (
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <>
+      <GlobalStyle />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </>
   );
 }
 
