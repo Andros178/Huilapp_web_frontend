50f3df33e96f4f7879006d81b6195327e1112948
Angie laiseca <150633452+AngieLaiseca24@users.noreply.github.com>
2025-11-28 10:03:40 -0500

    Prueba

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <h1>VERSION DESPLIEGUE PRUEBA</h1>
     <App />
   </React.StrictMode>
 );
